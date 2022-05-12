038027efaf4090f3651e22de15334cf4c15529e8
Jacky <38830587+kaij1941@users.noreply.github.com>
2022-05-12 14:17:01 +0800

    Update postModel.js

---
+++ models/postModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const postSchema =new mongoose.Schema({
+    articleContent:{
+        type:String,
+        requird:[true,'貼文內容未填寫']
+    },
+    articlePhoto:{
+        type:String,
+    },
+    userName:{
+        type:String,
+        requird:[true,'貼文者名稱未填寫']
+    },
+    userPhoto:{
+        type:String,
+        requird:[true,'貼文者照片未填寫']
+    },
+    likes:{
+        type:Number,
+        requird:[true,'按讚數為必填,至少為零']
+    },
+    creatTime:{
+        type:Date,
+        default:Date.now,
+        select:false
+    }
+})
+
+
+const postModel = mongoose.model('post',postSchema);
+
+module.exports = postModel;
